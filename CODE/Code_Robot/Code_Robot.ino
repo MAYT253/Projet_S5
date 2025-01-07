01a9d8cf2a3cec6f60afc1ae8ff2405736293926
Tristan-Mercier <merciertristan29@gmail.com>
2025-01-07 11:00:17 +0100

    Update Code_Robot.ino

--- CODE/Code_Robot/Code_Robot.ino
+++ CODE/Code_Robot/Code_Robot.ino
@@ -17,7 +17,10 @@ Encoder codeur_L(18, 29);
 Encoder codeur_R(19, 27);
 
 ISR(TIMER5_OVF_vect) {
-  if (compteur++ >=244 && code_arret == 0) mesure_energie();
+  if (compteur++ >= 244 && code_arret == 0) {
+    mesure_energie();
+    compteur = 0;
+  }
 }
 
 void setup() {
@@ -30,18 +33,17 @@ void setup() {
 void loop() {
   temps_start = millis();
   while (code_arret == 0) {
-  code_arret = Arret(moteur.CO_L, moteur.CO_R, codeur_L.read(), codeur_R.read());
+    code_arret = Arret(moteur.CO_L, moteur.CO_R, codeur_L.read(), codeur_R.read());
     moteur.POLICE();
     detect_mur();
     lcd.setCursor(0, 0);
-    lcd.print(code_arret);
+    lcd.print(energie);
     //CODE ICI
-    moteur.MoteurGD(600,600);
-
+    moteur.MoteurGD(600, 600);
   }
   while (1) {
     delay(100);
-    affichage(code_arret, (millis()-temps_start)/1000, energie, nb_mur, codeur_L.read(), codeur_R.read());
+    affichage(code_arret, (millis() - temps_start) / 1000, energie, nb_mur, codeur_L.read(), codeur_R.read());
     moteur.Stop();
   }
 }
@@ -50,10 +52,10 @@ void detect_mur() {
   if (UltraF.MeasureInCentimeters() <= 20 || UltraD.MeasureInCentimeters() <= 20 || UltraG.MeasureInCentimeters() <= 20) nb_mur++;
 }
 
-void mesure_energie(){
+void mesure_energie() {
   float courant;
-  for (int i = 0;i < 10; i++) courant = map(analogRead(A5), 0, 1023, MIN_CURRENT, MAX_CURRENT) + map(analogRead(A4), 0, 1023, MIN_CURRENT, MAX_CURRENT);
-  courant = courant/10;
-	float tension = (3 * analogRead(A0) * 2490 / 1023.00);
-  energie = energie + courant*tension;
+  for (int i = 0; i < 10; i++) courant = map(analogRead(A5), 0, 1023, MIN_CURRENT, MAX_CURRENT) + map(analogRead(A4), 0, 1023, MIN_CURRENT, MAX_CURRENT);
+  courant = courant / 10;
+  float tension = (3 * analogRead(A0) * 2490 / 1023.00);
+  energie = energie + courant * tension;
 }
