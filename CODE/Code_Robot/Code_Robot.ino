c756e679d72b0d6658647f7a061acdd8cbbe3b1b
Tristan-Mercier <merciertristan29@gmail.com>
2025-01-07 10:11:14 +0100

    Update Code_Robot.ino

--- CODE/Code_Robot/Code_Robot.ino
+++ CODE/Code_Robot/Code_Robot.ino
@@ -5,15 +5,19 @@
 
 moteur moteur;
 
+int nb_mur;
 int code_arret;
+long temps_parcours;
+float energie;
+int compteur = 0;
+long temps_start;
 
 //Encoder
-static Encoder codeur_L(18, 29);
-static Encoder codeur_R(19, 27);
-
-int nb_mur;
+Encoder codeur_L(18, 29);
+Encoder codeur_R(19, 27);
 
 ISR(TIMER5_OVF_vect) {
+  if (compteur++ >=244 && code_arret == 0) mesure_energie();
 }
 
 void setup() {
@@ -24,20 +28,32 @@ void setup() {
 }
 
 void loop() {
-  while (1){//code_arret == 0) {
+  temps_start = millis();
+  while (code_arret == 0) {
   code_arret = Arret(moteur.CO_L, moteur.CO_R, codeur_L.read(), codeur_R.read());
     moteur.POLICE();
     detect_mur();
+    lcd.setCursor(0, 0);
+    lcd.print(code_arret);
     //CODE ICI
     moteur.MoteurGD(600,600);
+
   }
   while (1) {
     delay(100);
-    affichage(code_arret, 0, 0, nb_mur, codeur_L.read(), codeur_R.read());
+    affichage(code_arret, (millis()-temps_start)/1000, energie, nb_mur, codeur_L.read(), codeur_R.read());
     moteur.Stop();
   }
 }
 
 void detect_mur() {
   if (UltraF.MeasureInCentimeters() <= 20 || UltraD.MeasureInCentimeters() <= 20 || UltraG.MeasureInCentimeters() <= 20) nb_mur++;
+}
+
+void mesure_energie(){
+  float courant;
+  for (int i = 0;i < 10; i++) courant = map(analogRead(A5), 0, 1023, MIN_CURRENT, MAX_CURRENT) + map(analogRead(A4), 0, 1023, MIN_CURRENT, MAX_CURRENT);
+  courant = courant/10;
+	float tension = (3 * analogRead(A0) * 2490 / 1023.00);
+  energie = energie + courant*tension;
 }
